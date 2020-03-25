9fe8fe2ecfd46d8e1c660d5d78bd48c175e6ed30
We'Am <weaam.wewe91@gmail.com>
2020-03-25 10:25:25 +0200

    Edited Events for data

--- Restaurant/PromotionEvent.cpp
+++ Restaurant/PromotionEvent.cpp
@@ -1,6 +1,8 @@
 #include "PromotionEvent.h"
 #include "Rest/Order.h"
 #include "Rest/Restaurant.h"
+
+
 PromotionEvent::PromotionEvent(int oTime, int oID):Event(oTime,oID)
 {
 	this->oID = oID;
@@ -16,7 +18,8 @@ PromotionEvent::PromotionEvent(int oTime, int oID, double Money):Event(oTime,oID
 void PromotionEvent::Execute(Restaurant* pRest)
 {
 	Order* ORD = pRest->GetNormalOrderByID(oID);
-	pRest->AddtoVIPQueue(ORD, -1); // -1 Is an indication that it is added to the end of the queue as the priorties are all postitive
-
-	
+	ORD->AddExtraMoney(ExMONY);
+	ORD->SetType(TYPE_VIP);
+	int Equation = ORD->GettotalMoney() + (1000 / (float)ORD->GetArrTime()) + ORD->GetSize();
+	pRest->AddtoVIPQueue(ORD, -1); // -1 Is an indication that it is added to the end of the queue as the priorties are all postitive	
 }
